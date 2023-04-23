1903711a088ecb95ec0c5d34d414782070b3ed8a
Your Name <you@example.com>
2023-04-23 20:18:33 +0200

    domaci_ukol_04_bonus

---
+++ domaci_ukol_04_bonus.py
@@ -0,0 +1,30 @@
+tel_cislo = input("Zadej telefonni cislo:")
+
+def overeni_tel_cisla(zadane_cislo) -> int:
+    cislo = zadane_cislo.replace(" ", "")
+    if len(cislo) == 9:
+        hodnota = True
+    elif len(cislo) == 13:
+        if cislo[0:4] == "+420":
+            hodnota = True
+        else:
+            hodnota = False
+    else:
+        hodnota = False
+    return hodnota
+
+
+spravnoste_tel_cisla = overeni_tel_cisla(tel_cislo)
+if spravnoste_tel_cisla == False:
+    print(f"Zadali jste nespravny format telefonniho cisla.")
+else:
+    zadana_zprava = input("Zadej zpravu, kterou chcete poslat:")
+
+def vypocet_ceny(zprava):
+    zakladni_cena = 3
+    delka = len(zprava)//180
+    cena = zakladni_cena + (delka * 3)
+    return cena
+
+vysledna_cena = vypocet_ceny(zadana_zprava)
+print(f"Za vasi zpravu zaplatite: {vysledna_cena} Kc.")
